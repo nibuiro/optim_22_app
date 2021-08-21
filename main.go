7fda32363b40f8bd262178b892861071a1c4a197
川田剣士 <kenji.k.4506@gmail.com>
2021-08-21 19:31:27 +0900

    edit the main.go file a little

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import (
   "net/http"
   "github.com/gin-gonic/gin"
+  // コメントを外す
   "optim_22_app/model"
 )
 
 func main() {
   // DB接続後、マイグレーションを実行する。
+  // 手順としては、まずコンテナを立ち上げた後、mysqlでoptim_devデータベースを作成する。
+  // その後、model.InitDB(),import(optim_22_app/model)のコメントを外し、カレントディレクトリでgo run main.goを実行する。
+  // プログラムの詳細はmodel/migrate.goに記載。
   model.InitDB()
+
   // ルーターを作成している
   router := gin.Default()
   // helloメソッドがwebブラウザから指定された場合、Hello World!!を返す。
