3c1434cb9b4de62cc64d65dc5c9ecb7005d88000
NEUTRON <neutron0831@gmail.com>
2021-09-04 00:27:58 +0900

    Add HomePage.vue

---
+++ view/src/components/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="homepage">
+    <p>This is homepage.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
