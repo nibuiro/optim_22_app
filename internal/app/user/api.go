d6dd8851e316920061b3abb8bdbf9bc955405833
nibuiro <immay1999@gmail.com>
2021-09-09 16:15:37 +0900

    Define user/api.go

--- internal/app/user/api.go
+++ internal/app/user/api.go
@@ -6,23 +6,31 @@ import (
   "optim_22_app/pkg/log"
 )
 
-// RegisterHandlers sets up the routing of the HTTP handlers.
-func RegisterHandlers(r *gin.RouterGroup, service Service, logger log.Logger) { //, authHandler routing.Handler
-  res := resource{service, logger}
-
-//  r.Get("/user/<id>", res.get)
-//  r.Get("/user", res.query)
-//
-//  //r.Use(authHandler)
-//
-  r.POST("/user", res.stubCreateUser)
-//  r.Put("/user/<id>", res.update)
-//  r.Delete("/user/<id>", res.delete)
+//`POST /api/user`が要求する情報
+type registrationInformation struct {
+  name     string `json:"name"`
+  email    string `json:"email"`
+  password string `json:"password"`
 }
 
+//ユーザ操作の依存関係
 type resource struct {
   service Service
   logger  log.Logger
 }
 
-func (r resource) stubCreateUser(c *gin.Context) { c.Status(http.StatusCreated) }
+//ユーザ操作についてエンドポイントを登録
+func RegisterHandlers(r *gin.RouterGroup, service Service, logger log.Logger) {
+
+  res := resource{service, logger}
+
+  //登録する
+  r.POST("/api/user", res.create)
+  //退会する
+  r.DELETE("/api/user", res.delete)
+  //ログイン
+  r.POST("/api/session", res.login)
+  //ログアウト
+  r.DELETE("/api/session", res.logout)
+
+}
