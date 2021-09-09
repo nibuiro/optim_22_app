888147b3314a774b8aa1032829fb7964fa12a2aa
nibuiro <immay1999@gmail.com>
2021-09-09 23:54:53 +0900

    Add user/api/create()

--- internal/app/user/api.go
+++ internal/app/user/api.go
@@ -4,6 +4,7 @@ import (
   "github.com/gin-gonic/gin"
   "net/http"
   "optim_22_app/pkg/log"
+  "optim_22_app/internal/pkg/Config"
 )
 
 //`POST /api/user`が要求する情報
@@ -15,6 +16,7 @@ type registrationInformation struct {
 
 //ユーザ操作の依存関係
 type resource struct {
+  config Config
   service Service
   logger  log.Logger
 }
@@ -22,15 +24,41 @@ type resource struct {
 //ユーザ操作についてエンドポイントを登録
 func RegisterHandlers(r *gin.RouterGroup, service Service, logger log.Logger) {
 
-  res := resource{service, logger}
+  rc := resource{service, logger}
 
   //登録する
-  r.POST("/api/user", res.create)
+  r.POST("/api/user", rc.create)
   //退会する
-  r.DELETE("/api/user", res.delete)
+  r.DELETE("/api/user", rc.delete)
   //ログイン
-  r.POST("/api/session", res.login)
+  r.POST("/api/session", rc.login)
   //ログアウト
-  r.DELETE("/api/session", res.logout)
+  r.DELETE("/api/session", rc.logout)
 
 }
+
+
+func (rc resource) create(c *gin.Context) error {
+
+  var input registrationInformation
+
+  //BOdyからJSONをパースして読み取る
+  if err := c.BindJSON(&input); err != nil {
+    return err
+  }
+
+  //ユーザ作成及び認証情報取得
+  refreshToken, accessToken, err := rc.service.Create(c.Request.Context(), input)
+  if err != nil {
+    return err
+  }
+  
+  //#region ヘッダに認証情報を付加
+  c.Header("Authorization", accessToken)
+  c.SetCookie(name, refreshToken, 1, "/",  rc.config.domain, false, true)
+  c.Status(http.StatusCreated)
+  //#endregion
+}
+
+
+
