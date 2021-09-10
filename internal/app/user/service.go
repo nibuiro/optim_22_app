7ed7c9bce60cbf8b341a58d392015bea3de6a4ab
nibuiro <immay1999@gmail.com>
2021-09-11 00:23:37 +0900

    Add user/service/Create()

--- internal/app/user/service.go
+++ internal/app/user/service.go
@@ -1,6 +1,10 @@
 package user
 
 import (
+  "regexp"
+  "github.com/go-ozzo/ozzo-validation/v4"
+  "github.com/go-ozzo/ozzo-validation/v4/is"
+  "optim_22_app/pkg/authentication"
   "optim_22_app/pkg/log"
   "optim_22_app/typefile"
   "context"
@@ -8,59 +12,51 @@ import (
 
 //`POST /api/user`が要求する情報
 type registrationInformation struct {
-  name     string `json:"name"`
-  email    string `json:"email"`
-  password string `json:"password"`
-}
-
-
-//`POST /api/session`が要求する情報
-type loginInformation struct {
-  email    string `json:"email"`
-  password string `json:"password"`
+  Name     string `json:"name"`
+  Email    string `json:"email"`
+  Password string `json:"password"`
 }
 
 
 type Service interface {
   Create(ctx context.Context, input registrationInformation) (string, string, error)
   Delete(ctx context.Context, userId int) error
-  Login(ctx context.Context, input loginInformation) (string, string, error)
 }
 
 type service struct {
+  auth   *authentication.Authorizer
   repo   Repository
   logger log.Logger
 }
 
 
-func (s service) Create(ctx context.Context, input registrationInformation) (string, string, error) {
-  return StubCreate()
+func (s service) Create(ctx context.Context, req registrationInformation) (string, string, error) {
+  //リクエストの値を検証
+  if err := req.Validate(); err != nil {
+    return "", "", err
+  }
+  //クエリの値を定義
+  insertValues := typefile.User{
+    ID:        0,         //無視される、nil `gorm:"primaryKey;autoIncrement:true"`
+    Name:      req.Name,
+    Email:     req.Email,
+    Password:  req.Password,
+  }
+  //INSERTと割り当てられるuserIDを取得
+  if err := s.repo.Create(ctx, &insertValues); err != nil {
+    return "", "", err
+  } else {
+    userId := insertValues.ID
+  }
+  //トークン発行
+  refreshToken, accessToken := auth.AuthorizationService.New(userId)
+
+  return refreshToken, accessToken, nil
 }
-func (s service) Delete(ctx context.Context, userId int) error {
-  return StubDelete()
-}
-func (s service) Login(ctx context.Context, input loginInformation) (string, string, error) {
-  return StubLogin()
-}
 
 
 
-
-type User struct {
-  typefile.User
-}
-
-type CreateUserRequest struct {
-  typefile.User
-}
-
-
+func StubNewService(args ...interface{}) Service { return service{nil, nil}}
 func StubCreate(args ...interface{}) (string, string, error)  {return "", "", nil}
 func StubDelete(args ...interface{}) error {return nil}
 func StubLogin(args ...interface{}) (string, string, error) {return "", "", nil}
-
-
-
-func StubNewService(args ...interface{}) Service {
-  return service{nil, nil}
-}
