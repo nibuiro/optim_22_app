9dba2ae3ae98c73f8b5b01dc1f9daea7308c8f24
nibuiro <immay1999@gmail.com>
2021-09-11 13:28:02 +0900

    Add user/repository/Create()

--- internal/app/user/repository.go
+++ internal/app/user/repository.go
@@ -1,17 +1,29 @@
 package user
 
 import (
+  
+  "gorm.io/gorm"
   "optim_22_app/pkg/log"
 )
 
 
 type Repository interface {
+  Create(ctx context.Context, user *typefile.User) error
+  Delete(ctx context.Context, userId string) error
 }
 
+
 type repository struct {
-//  db     *dbcontext.DB
+  db     *gorm.DB
   logger log.Logger
 }
+
+
+func (r Repository) Create(ctx context.Context, user *typefile.User) error {
+  result := r.db.WithContext(ctx).Create(user)
+  return result.Error
+}
+
 
 func StubNewRepository(args ...interface{}) Repository {
   return repository{nil}
