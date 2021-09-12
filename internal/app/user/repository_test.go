12fa5e539b909892b8cb3e91b75981b62a2325f7
nibuiro <immay1999@gmail.com>
2021-09-12 11:16:09 +0900

    Add comment

--- internal/app/user/repository_test.go
+++ internal/app/user/repository_test.go
@@ -1,55 +1,65 @@
 package user
 
 import (
-
-    "context"
-    "testing"
-    "regexp"
-    "github.com/DATA-DOG/go-sqlmock"
-    "github.com/stretchr/testify/suite"
-    "gorm.io/driver/postgres"
-    "gorm.io/gorm"
+  "context"
+  "testing"
+  "regexp"
+  "github.com/DATA-DOG/go-sqlmock"
+  "github.com/stretchr/testify/suite"
+  "gorm.io/driver/postgres"
+  "gorm.io/gorm"
   "optim_22_app/pkg/log"
   "optim_22_app/typefile"
 
 )
 
+/*
+ *  mysqlドライバに適合した書き方が不明。
+ *  postgresドライバで代用
+ *
+ *  参考：
+ *　[INSERT while mocking gorm · Issue #118 · DATA-DOG/go-sqlmock · GitHub]
+ *  (https://github.com/DATA-DOG/go-sqlmock/issues/118)
+ *
+ */
+
 
 // テストスイートの構造体
 type UserRepositoryTestSuite struct {
-    suite.Suite
-    userRepository repository
-    mock           sqlmock.Sqlmock
+  suite.Suite
+  userRepository repository
+  mock           sqlmock.Sqlmock
 }
 
 // テストのセットアップ
 // (sqlmockをNew、Gormで発行されるクエリがモックに送られるように)
 func (suite *UserRepositoryTestSuite) SetupTest() {
-    db, mock, _ := sqlmock.New()
-    suite.mock = mock
-    logger := log.New()
-    DB, _ := gorm.Open(postgres.New(postgres.Config{
-        Conn: db,
-    }), &gorm.Config{})
-    userRepository := repository{DB, logger}
-    suite.userRepository = userRepository
+  db, mock, _ := sqlmock.New()
+  suite.mock = mock
+  logger := log.New()
+  DB, _ := gorm.Open(
+    postgres.New(postgres.Config{Conn: db,}), 
+    &gorm.Config{},
+  )
+  userRepository := repository{DB, logger}
+  suite.userRepository = userRepository
 }
 
 // テスト終了時の処理（データベース接続のクローズ）
 func (suite *UserRepositoryTestSuite) TearDownTest() {
-    db, _ := suite.userRepository.db.DB()
-    db.Close()
+  db, _ := suite.userRepository.db.DB()
+  db.Close()
 }
 
 // テストスイートの実行
 func TestUserRepositoryTestSuite(t *testing.T) {
-    suite.Run(t, new(UserRepositoryTestSuite))
+  suite.Run(t, new(UserRepositoryTestSuite))
 }
 
 // Createのテスト
 func (suite *UserRepositoryTestSuite) TestCreate() {
   suite.Run("create a user", func() {
-      newId := 0
+      newId := 1
       rows := sqlmock.NewRows([]string{"id"}).AddRow(newId)
       suite.mock.ExpectBegin()
       suite.mock.ExpectQuery(
@@ -58,13 +68,13 @@ func (suite *UserRepositoryTestSuite) TestCreate() {
       WillReturnRows(rows)
       suite.mock.ExpectCommit()
       
-      insertValues := &typefile.User{  //無視される、nil `gorm:"primaryKey;autoIncrement:true"`
-      Name:      "test",
-      Email:     "test@test.test",
-      Password:  "test",
+      insertValues := &typefile.User{
+        Name:      "test",
+        Email:     "test@test.test",
+        Password:  "test",
       }
       
-      var ctx    context.Context
+      var ctx context.Context
       err := suite.userRepository.Create(ctx, insertValues)
     
       suite.userRepository.logger.Debug(insertValues)
