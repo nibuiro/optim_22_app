b6be97ecf614af1086406271310805d1bf05acbd
nibuiro <immay1999@gmail.com>
2021-09-16 16:18:27 +0900

    Add TestPostProfile()

---
+++ internal/app/profile/api_test.go
@@ -0,0 +1,51 @@
+package profile
+
+import (
+  "net/http"
+  "testing"
+  "github.com/gin-gonic/gin"
+  "optim_22_app/internal/pkg/test"
+  "optim_22_app/pkg/log"
+  "optim_22_app/internal/pkg/config"
+)
+  
+/* 
+ * エンドポイントとして機能するか？
+ */
+
+//望ましい利用手順でテスト
+func TestPostProfile(t *testing.T) {
+
+  logger := log.New()
+  router := gin.New()
+  cfg, _ := config.Load("/go/src/configs/app.yaml", logger)
+
+  repo := StubNewRepository()
+  RegisterHandlers(router.Group(""), cfg, StubNewService(repo), logger)
+  
+  tests := []test.APITestCase{
+    {
+      Name: "Nested json parsing test", 
+      Method: "POST", 
+      URL: "/api/profile", 
+      Header: nil, 
+      Body: `{"bio":"test", "sns":{"twitter": "twitter.com/pole", "facebook": "facebook.com/pole"}, "submission":"test", "request":"test", "icon":"test"}`,
+      WantStatus: http.StatusOK, 
+      WantResponse: "",
+    }, 
+    {
+      Name: "Bad json parsing test", 
+      Method: "POST", 
+      URL: "/api/profile", 
+      Header: nil, 
+      Body: `"bio":"test", "sns":{"twitter": "twitter.com/pole", "facebook": "facebook.com/pole"}, "submission":"test", "request":"test", "icon":"test"`,
+      WantStatus: http.StatusBadRequest, 
+      WantResponse: "",
+    },
+  }
+  for _, tc := range tests {
+    test.Endpoint(t, router, tc)
+  }
+}
+
+
