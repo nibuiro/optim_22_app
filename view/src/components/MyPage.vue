28013d24b6e451dd6d3f651dbd01655364ac129e
NEUTRON <neutron0831@gmail.com>
2021-09-20 02:38:43 +0900

    Add MyPage.vue

---
+++ view/src/components/MyPage.vue
@@ -0,0 +1,243 @@
+<template>
+  <div class="container" :profile="profile">
+    <section class="hero is-primary is-small mb-3">
+      <b-tooltip
+        style="position: absolute;"
+        :label="profile.comment"
+        type="is-light"
+        position="is-right"
+        always
+      >
+        <div class="ml-3 mt-3 mb-6" :style="iconStyle" />
+      </b-tooltip>
+      <div class="hero-body is-flex pt-0 pb-5">
+        <p class="title mb-0 pt-2" style="margin-left: 64px;">
+          {{ profile.username }}
+        </p>
+        <b-button class="is-light ml-auto mt-5" label="編集" outlined />
+      </div>
+    </section>
+    <section class="mb-3 is-flex is-justify-content-space-evenly">
+      <b-taglist class="m-0" attached>
+        <b-tag style="background-color: #171516;">
+          <b-icon icon="github" type="is-light" />
+        </b-tag>
+        <b-tag type="is-light">
+          <a href="">@{{ profile.SNS.Github }}</a>
+        </b-tag>
+      </b-taglist>
+      <b-taglist class="m-0" attached>
+        <b-tag style="background-color: #1D9BF0;">
+          <b-icon icon="twitter" type="is-light" />
+        </b-tag>
+        <b-tag type="is-light">
+          <a href="">@{{ profile.SNS.Twitter }}</a>
+        </b-tag>
+      </b-taglist>
+      <b-taglist class="m-0" attached>
+        <b-tag style="background-color: #1877F2;">
+          <b-icon icon="facebook" type="is-light" />
+        </b-tag>
+        <b-tag type="is-light">
+          <a href="">@{{ profile.SNS.Facebook }}</a>
+        </b-tag>
+      </b-taglist>
+    </section>
+    <section class="mb-3">
+      <b-tabs type="is-boxed">
+        <b-tab-item>
+          <template #header>
+            <b-icon icon="account-question-outline"></b-icon>
+            <span>
+              過去にした依頼
+              <b-tag rounded> {{ profile.requests.length }} </b-tag>
+            </span>
+          </template>
+          <b-table :data="profile.requests" :default-sort="['date', 'desc']">
+            <b-table-column
+              field="state"
+              label="状態"
+              width="10%"
+              centered
+              v-slot="props"
+            >
+              <b-tag
+                :type="
+                  props.row.accepting === true ? 'is-success' : 'is-danger'
+                "
+              >
+                {{ props.row.accepting === true ? "受付中" : "終了" }}
+              </b-tag>
+            </b-table-column>
+
+            <b-table-column
+              field="date"
+              label="依頼日時"
+              width="10%"
+              sortable
+              centered
+              v-slot="props"
+            >
+              {{ new Date(props.row.date).toLocaleDateString() }}
+              <br />
+              {{ new Date(props.row.date).toLocaleTimeString() }}
+            </b-table-column>
+
+            <b-table-column
+              field="request"
+              label="依頼名"
+              width="20%"
+              sortable
+              v-slot="props"
+            >
+              <a href="">
+                {{ props.row.request }}
+              </a>
+            </b-table-column>
+
+            <b-table-column
+              field="detail"
+              label="詳細"
+              width="35%"
+              v-slot="props"
+            >
+              {{ props.row.detail }}
+            </b-table-column>
+
+            <b-table-column
+              field="engineer"
+              label="参加者"
+              width="25%"
+              v-slot="props"
+            >
+              <a
+                href=""
+                v-for="engineer in props.row.engineers"
+                :key="engineer.username"
+              >
+                <b-tooltip :label="engineer.username">
+                  <b-image
+                    class="image is-48x48 is-inline-block is-centered has-border"
+                    :src="engineer.icon"
+                    rounded
+                  />
+                </b-tooltip>
+              </a>
+            </b-table-column>
+          </b-table>
+        </b-tab-item>
+        <b-tab-item>
+          <template #header>
+            <b-icon icon="account-edit-outline"></b-icon>
+            <span>
+              過去に受けた依頼
+              <b-tag rounded> {{ profile.submissions.length }} </b-tag>
+            </span>
+          </template>
+          <b-table :data="profile.submissions" :default-sort="['date', 'desc']">
+            <b-table-column
+              field="state"
+              label="状態"
+              width="10%"
+              centered
+              v-slot="props"
+            >
+              <b-tag
+                :type="
+                  props.row.accepting === true ? 'is-success' : 'is-danger'
+                "
+              >
+                {{ props.row.accepting === true ? "受付中" : "終了" }}
+              </b-tag>
+            </b-table-column>
+
+            <b-table-column
+              field="date"
+              label="依頼日時"
+              width="10%"
+              sortable
+              centered
+              v-slot="props"
+            >
+              {{ new Date(props.row.date).toLocaleDateString() }}
+              <br />
+              {{ new Date(props.row.date).toLocaleTimeString() }}
+            </b-table-column>
+
+            <b-table-column
+              field="client"
+              label="依頼者"
+              width="10%"
+              centered
+              v-slot="props"
+            >
+              <a href="">
+                <b-tooltip :label="props.row.client.username">
+                  <b-image
+                    class="image is-64x64 is-inline-block is-centered"
+                    :src="props.row.client.icon"
+                    rounded
+                  />
+                </b-tooltip>
+              </a>
+            </b-table-column>
+
+            <b-table-column
+              field="request"
+              label="依頼名"
+              width="20%"
+              sortable
+              v-slot="props"
+            >
+              <a href="">
+                {{ props.row.request }}
+              </a>
+            </b-table-column>
+
+            <b-table-column
+              field="detail"
+              label="詳細"
+              width="30%"
+              v-slot="props"
+            >
+              {{ props.row.detail }}
+            </b-table-column>
+
+            <b-table-column
+              field="engineer"
+              label="提出物"
+              width="20%"
+              v-slot="props"
+            >
+              <a href="">提出物</a>
+            </b-table-column>
+          </b-table>
+        </b-tab-item>
+      </b-tabs>
+    </section>
+  </div>
+</template>
+
+<script>
+const profile = require("../../src/assets/sampleProfile.json");
+
+export default {
+  data() {
+    return {
+      profile,
+      iconStyle: {
+        width: "64px",
+        height: "64px",
+        backgroundImage: `url("${profile.icon}")`,
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        borderRadius: "100%"
+      }
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
--- view/src/router/index.js
+++ view/src/router/index.js
@@ -1,14 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HomePage from '@/components/HomePage'
+import MyPage from '@/components/MyPage'
 
 Vue.use(Router)
 
 export default new Router({
     mode: 'history',
     routes: [{
-        path: '/',
-        name: 'HomePage',
-        component: HomePage
-    }]
+            path: '/',
+            name: 'HomePage',
+            component: HomePage
+        },
+        {
+            path: '/users/:userid',
+            name: 'MyPage',
+            component: MyPage
+        }
+    ]
 })
