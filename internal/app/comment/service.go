c64a14e1aeda82c7d30cce7f5f64800048e33c35
nibuiro <immay1999@gmail.com>
2021-09-20 16:18:33 +0900

    Add Comment.Validate()

--- internal/app/comment/service.go
+++ internal/app/comment/service.go
@@ -12,6 +12,16 @@ type Comment struct {
 }
 
 
+func (m Comment) Validate() error {
+  return validation.ValidateStruct(&m,
+    validation.Field(&m.UserID, validation.Required, is.Int),
+    validation.Field(&m.Date, validation.Required, validation.Date("2006-01-02")),
+    validation.Field(&m.Title, validation.Required, validation.Length(3, 64)),
+    validation.Field(&m.Text, validation.Required, validation.Length(3, 128)),
+    validation.Field(&m.ReplyID, validation.Required, is.Int),
+    //validation.Field(&m.Attachment, validation.Length(3, 128)),
+  )
+}
 //#endregion
 
 
