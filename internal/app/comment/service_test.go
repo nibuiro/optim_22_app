3327a21f30df5859ac5675c92ae13b7bbc90ba78
nibuiro <immay1999@gmail.com>
2021-09-23 23:45:18 +0900

    Add TestTypeCommentValidate

--- internal/app/comment/service_test.go
+++ internal/app/comment/service_test.go
@@ -1,1 +1,72 @@
 package comment
+
+import (
+  "testing"
+  "time"
+  "strings"
+//  "github.com/gin-gonic/gin"
+//  "optim_22_app/internal/pkg/test"
+//  "optim_22_app/pkg/log"
+//  "optim_22_app/typefile"
+  "github.com/stretchr/testify/assert"
+  "optim_22_app/pkg/log"
+)
+  
+
+func TestTypeCommentValidate(t *testing.T) {
+  /*
+   *  主にutf8のカウント検証テスト
+   *  
+   */
+  logger := log.New()
+  t1, _ := time.Parse(time.RFC3339, "2009-11-12 21:00:57")
+  tests := []struct {
+    name      string
+    model     comment
+    wantError bool
+  }{
+    {
+      "normal query", 
+      comment{
+      UserID: 1,
+      RequestID: 1,
+      Date: t1,
+      Title: strings.Repeat("あ", 64),
+      Body: strings.Repeat("あ", 128),
+      ReplyID: 0,
+      }, 
+      true,
+    },
+    {
+      "long title", 
+      comment{
+      UserID: 1,
+      RequestID: 1,
+      Date: t1,
+      Title: strings.Repeat("あ", 65),
+      Body: strings.Repeat("あ", 128),
+      ReplyID: 0,
+      }, 
+      true,
+    },
+    {
+      "long body", 
+      comment{
+      UserID: 1,
+      RequestID: 1,
+      Date: t1,
+      Title: strings.Repeat("あ", 64),
+      Body: strings.Repeat("あ", 129),
+      ReplyID: 0,
+      }, 
+      true,
+    },
+  }
+  for _, tt := range tests {
+    t.Run(tt.name, func(t *testing.T) {
+      err := tt.model.Validate()
+      logger.Debug(err)
+      assert.Equal(t, tt.wantError, err != nil)
+    })
+  }
+}
