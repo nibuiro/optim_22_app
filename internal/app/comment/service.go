0d50f086b909d1acbb00065edff701d6dc38553d
nibuiro <immay1999@gmail.com>
2021-09-23 10:10:16 +0900

    Change comment text name Text => Body

--- internal/app/comment/service.go
+++ internal/app/comment/service.go
@@ -2,13 +2,13 @@ package comment
 
 //#region コメント
 type Comment struct {
-  Id         int    `json:"id"`
-  RequestID  int    `json:"requestID"`
-  UserID     int    `json:"userID"`
+  Id         int       `json:"id"`
+  RequestID  int       `json:"requestID"`
+  UserID     int       `json:"userID"`
   Date       time.Time `json:"date"`
   Title      string    `json:"title"`
-  Text       string    `json:"text"`
-  ReplyID    int    `json:"replyID"`
+  Body       string    `json:"body"`
+  ReplyID    int       `json:"replyID"`
   Attachment []byte    `json:"attachment"`
 }
 
@@ -19,7 +19,7 @@ func (m Comment) Validate() error {
     validation.Field(&m.RequestID, validation.Required, is.Int),
     validation.Field(&m.Date, validation.Required, validation.Date("2006-01-02")),
     validation.Field(&m.Title, validation.Required, validation.Length(3, 64)),
-    validation.Field(&m.Text, validation.Required, validation.Length(3, 128)),
+    validation.Field(&m.Body, validation.Required, validation.Length(3, 128)),
     validation.Field(&m.ReplyID, validation.Required, is.Int),
     //validation.Field(&m.Attachment, validation.Length(3, 128)),
   )
