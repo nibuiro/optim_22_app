a0a8bb632777eb642995429cb0a9e0a7f535d414
nibuiro <immay1999@gmail.com>
2021-09-24 22:57:02 +0900

    Implement app/comment/api/get()

--- internal/app/comment/api.go
+++ internal/app/comment/api.go
@@ -1,12 +1,15 @@
 package comment
 
 import (
-//  "net/http"
+  "net/http"
+  "strconv"
+  "encoding/json"
   "github.com/gin-gonic/gin"
   "optim_22_app/pkg/log"
   "optim_22_app/internal/pkg/config"
+ // b64 "encoding/base64"
 //  "optim_22_app/internal/pkg/utils"
-
+//  "reflect"
 )
 
 //コメント操作の依存関係
@@ -22,11 +25,48 @@ func RegisterHandlers(r *gin.RouterGroup, config *config.Config, service Service
   rc := resource{config, service, logger}
 
   //ディスカッション ID(:requestID) のコメント一覧を取得
-  r.GET("/api/discussion/:requestID", rc.getStub())
+  r.GET("/api/discussion/:requestID", rc.get())
   //ディスカッション ID(:requestID) にコメントを投稿
   r.POST("/api/discussion/:requestID", rc.createStub())
   //ディスカッション ID(:requestID) に投稿されている
   //コメント ID(:commentID) を削除
   r.DELETE("/api/discussion/:requestID/:commentID", rc.deleteStub())
 
+}
+
+
+func (rc resource) get() gin.HandlerFunc {
+  return func(c *gin.Context) {
+    requestID := c.Param("requestID")
+
+    if !isIntegerString(requestID) {
+      c.Status(http.StatusBadRequest)
+      return
+    }
+
+    comments, err := rc.service.Get(c.Request.Context(), requestID)
+    //rc.logger.Debug(comments)
+    if err != nil {
+      rc.logger.Error(err)
+      c.Status(http.StatusNotFound)
+      return 
+    } else {
+      if commentsText, err := json.Marshal(comments); err != nil {
+        rc.logger.Error(err)
+        c.Status(http.StatusInternalServerError)
+        return
+      } else {
+        //rc.logger.Debug(reflect.TypeOf(commentsText))
+        c.Header("Content-Type", "application/json")
+        c.String(http.StatusOK, string(commentsText[:]))
+        return
+      }
+    }
+  }
+}
+
+
+func isIntegerString(query string) bool {
+  _, err := strconv.Atoi(query)
+  return err == nil
 }
