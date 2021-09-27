29256172cdab417d53fd3e4b7623a8deae2313e9
nibuiro <immay1999@gmail.com>
2021-09-27 22:38:57 +0900

    Remove config

--- internal/app/user/api.go
+++ internal/app/user/api.go
@@ -4,22 +4,20 @@ import (
   "net/http"
   "github.com/gin-gonic/gin"
   "optim_22_app/pkg/log"
-  "optim_22_app/internal/pkg/config"
   "optim_22_app/internal/pkg/utils"
 
 )
 
 //ユーザ操作の依存関係
 type resource struct {
-  config *config.Config
   service Service
   logger  log.Logger
 }
 
 //ユーザ操作についてエンドポイントを登録
-func RegisterHandlers(r *gin.RouterGroup, config *config.Config, service Service, logger log.Logger) {
+func RegisterHandlers(r *gin.RouterGroup, service Service, logger log.Logger) {
 
-  rc := resource{config, service, logger}
+  rc := resource{service, logger}
 
   //登録する
   r.POST("/api/user", rc.create())
