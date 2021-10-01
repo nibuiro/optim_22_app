18f4ed4a01c82be1303407230fc0710590a0598a
NEUTRON <neutron0831@gmail.com>
2021-10-01 15:30:34 +0900

    Add SubmissionPage.vue

---
+++ view/src/components/SubmissionPage.vue
@@ -0,0 +1,93 @@
+<!-- サブミッション詳細ページ -->
+
+<template>
+  <div class="container" :request="request" :submission="submission">
+    <section class="hero is-primary is-small mb-3">
+      <b-tooltip
+        style="position: absolute;"
+        :label="submission.comment"
+        type="is-light"
+        position="is-right"
+        always
+      >
+        <div
+          class="ml-3 mt-3 mb-6"
+          :style="iconStyle(64, submission.engineer.icon)"
+        />
+      </b-tooltip>
+      <div class="hero-body is-flex pt-0 pb-5">
+        <p class="title mb-0 pt-2" style="margin-left: 64px;">
+          {{ submission.engineer.username }}さんの提出物
+        </p>
+        <request-form class="is-light ml-auto mt-5" :request="request" />
+      </div>
+    </section>
+    <section class="mb-3">
+      <b-tabs type="is-boxed">
+        <b-tab-item>
+          <template #header>
+            <b-icon icon="file-upload-outline"></b-icon>
+            <span>提出物詳細</span>
+          </template>
+          <div class="content">
+            <ul :request="request" :submission="submission">
+              <li>
+                提出日時：
+                {{
+                  `${new Date(submission.date).toLocaleDateString()}
+                   ${new Date(submission.date).toLocaleTimeString()}`
+                }}
+              </li>
+              <li>依頼名　：{{ request.request }}</li>
+              <li>
+                提出物　：
+                <a href="">
+                  提出物のURL
+                </a>
+              </li>
+              <li>
+                コメント：
+                {{ submission.comment }}
+              </li>
+            </ul>
+          </div>
+        </b-tab-item>
+      </b-tabs>
+    </section>
+  </div>
+</template>
+
+<script>
+import RequestForm from "@/components/RequestForm";
+
+const submission = require("../../src/assets/sampleSubmission.json");
+const request = require("../../src/assets/sampleRequest.json");
+
+export default {
+  data() {
+    return {
+      submission,
+      request
+    };
+  },
+  methods: {
+    iconStyle(size, image) {
+      return {
+        width: `${size}px`,
+        height: `${size}px`,
+        backgroundImage: `url("${image}")`,
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        borderRadius: "100%"
+      };
+    }
+  },
+  components: {
+    "request-form": RequestForm
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
--- view/src/router/index.js
+++ view/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HomePage from '@/components/HomePage'
 import RequestPage from '@/components/RequestPage'
+import SubmissionPage from '@/components/SubmissionPage'
 import MyPage from '@/components/MyPage'
 
 Vue.use(Router)
@@ -17,6 +18,11 @@ export default new Router({
             path: '/request/:request_id',
             name: 'RequestPage',
             component: RequestPage
+        },
+        {
+            path: '/submission',
+            name: 'SubmissionPage',
+            component: SubmissionPage
         },
         {
             path: '/user/:user_id',
