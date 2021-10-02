2ef60dd9986ea976f72c08a859c4f7d6f961fc7d
NEUTRON <neutron0831@gmail.com>
2021-10-02 12:49:37 +0900

    Update DiscussionPage.vue

--- view/src/components/DiscussionPage.vue
+++ view/src/components/DiscussionPage.vue
@@ -2,49 +2,72 @@
 
 <template>
   <div class="container" :request="request">
-    <div v-for="comment in comments" :key="comment.comment_id" class="card">
-      <div class="card-content">
-        <div class="media">
-          <div class="media-content is-flex is-justify-content-space-between">
-            <span>{{ `${comment.comment_id}: ${comment.title}` }}</span>
-            <span>
-              {{
-                `${new Date(comment.createdat).toLocaleDateString()}
+    <section class="mb-3">
+      <div v-for="comment in comments" :key="comment.comment_id" class="card">
+        <div class="card-content">
+          <div class="media">
+            <div class="media-content is-flex is-justify-content-space-between">
+              <span>{{ `${comment.comment_id}: ${comment.title}` }}</span>
+              <span>
+                {{
+                  `${new Date(comment.createdat).toLocaleDateString()}
                 ${new Date(comment.createdat).toLocaleTimeString()}`
-              }}
-            </span>
+                }}
+              </span>
+            </div>
           </div>
-        </div>
-        <div class="is-flex">
-          <router-link
-            class="is-flex is-flex-direction-column"
-            :to="{
-              name: 'MyPage',
-              params: { user_id: comment.user.userid }
-            }"
-          >
-            <b-tooltip
-              type="is-light"
-              :label="comment.text"
-              position="is-right"
-              always
+          <div class="is-flex">
+            <router-link
+              class="is-flex is-flex-direction-column"
+              :to="{
+                name: 'MyPage',
+                params: { user_id: comment.user.userid }
+              }"
             >
               <div class="mx-auto" :style="iconStyle(32, comment.user.icon)" />
-            </b-tooltip>
-            <span>{{ comment.user.username }}</span>
-          </router-link>
+              <span>{{ comment.user.username }}</span>
+            </router-link>
+            <div class="content pl-3">
+              {{ comment.text }}
+            </div>
+          </div>
+          <span
+            v-if="!!comment.attachment"
+            class="is-flex is-justify-content-end"
+          >
+            <a class="is-flex is-align-items-center" :href="comment.attachment">
+              <b-icon icon="attachment" />
+              添付ファイル
+            </a>
+          </span>
         </div>
-        <span
-          v-if="!!comment.attachment"
-          class="is-flex is-justify-content-end"
-        >
-          <a class="is-flex is-align-items-center" :href="comment.attachment">
-            <b-icon icon="attachment" />
-            {{ comment.attachment }}
-          </a>
-        </span>
       </div>
-    </div>
+    </section>
+    <!-- ログイン済みなら -->
+    <section class="mt-5">
+      <article class="media">
+        <figure class="media-left">
+          <p class="image is-64x64">
+            <img src="https://bulma.io/images/placeholders/128x128.png" />
+          </p>
+        </figure>
+        <div class="media-content">
+          <b-field>
+            <div class="control">
+              <b-input
+                type="textarea"
+                :value="comment"
+                placeholder="コメントを入力する(500字以内)"
+                maxlength="500"
+              />
+              <div class="has-text-centered">
+                <b-button type="is-primary" label="送信" />
+              </div>
+            </div>
+          </b-field>
+        </div>
+      </article>
+    </section>
   </div>
 </template>
 
@@ -54,7 +77,8 @@ const comments = require("../../src/assets/sampleComments.json");
 export default {
   data() {
     return {
-      comments: comments.comments
+      comments: comments.comments,
+      comment: ""
     };
   },
   methods: {
