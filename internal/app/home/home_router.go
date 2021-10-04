cdb6e8c61c9a6c5b95309f29e0e85efe7e3294f4
川田剣士 <kenji.k.4506@gmail.com>
2021-10-04 16:30:55 +0900

    deal with new struct and edit the router files

--- internal/app/home/home_router.go
+++ internal/app/home/home_router.go
@@ -9,29 +9,34 @@ import (
 )
 
 //データを送信する際に利用する構造体を定義
+type UserProfileJson struct{
+	UserID            int                 `json:"user_id"`
+	UserName          string              `json:"username"`
+	Icon              string              `json:"icon"`
+	Bio               string              `json:"comment"`
+	Sns               string              `json:"SNS"`
+}
+
 type SubmissionJson struct{
-	ID             int                 `json:"submission_id"`
-	CreatedAt      time.Time           `json:"createdat"`
-	// 要件はエンジニアのプロフィールデータであるが、プロフィール機能は担当外のため、EngineerIDを代用する。
-	EngineerID     int                 `json:"engineer`
-	URL            string              `json:url`
-	Content        string              `json:"content"`
+	ID                int                 `json:"submission_id"`
+	CreatedAt         time.Time           `json:"createdat"`
+	EngineerProfile   UserProfileJson     `json:"engineer"`
+	URL               string              `json:url`
+	Content           string              `json:"content"`
 }
 
 type RequestJson struct{
-	ID             int                 `json:"request_id"`
-	RequestName    string              `json:"requestname"`
-	CreatedAt      time.Time           `json:"createdat"`
-	// 要件はクライアントのプロフィールデータであるが、プロフィール機能は担当外のため、ClientIDを代用する。
-	ClientID       int                 `json:"client"`
-	// 要件はエンジニアのプロフィールデータであるが、プロフィール機能は担当外のため、EngineerIDを代用する。
-	EngineersID    []int               `json:"engineers"`
-	Content        string              `json:"content"`
-	Submissions    []SubmissionJson    `json:"submissions"`
+	ID                int                 `json:"request_id"`
+	RequestName       string              `json:"requestname"`
+	CreatedAt         time.Time           `json:"createdat"`
+	ClientProfile     UserProfileJson     `json:"client"`
+	EngineersProfile  []UserProfileJson   `json:"engineers"`
+	Content           string              `json:"content"`
+	Submissions       []SubmissionJson    `json:"submissions"`
 }
 
 type RequestsJson struct{
-	Requests       []RequestJson       `json:"requests"`
+	Requests          []RequestJson       `json:"requests"`
 }
 
 // ホームページを表示する
@@ -48,6 +53,10 @@ func ShowHomepage(c *gin.Context) {
 	for _, request := range requests{
 		// RequestJson構造体データを格納するためのインスタンスを生成
 		request_json := RequestJson{}
+		// Client構造体を格納するためのインスタンスを生成
+		client := typefile.Client{}
+		// ClientのProfile構造体を格納するためのインスタンスを生成
+		client_profile := typefile.Profile{}
 		// Submission構造体データを複数格納するためのインスタンスを生成
 		submissions := []typefile.Submission{}
 		// Engineer構造体を複数格納するためのインスタンスを生成
@@ -56,6 +65,12 @@ func ShowHomepage(c *gin.Context) {
 		//　特定リクエストのidを持つsubmissionを格納する。
 		model.Db.Find(&submissions,"request_id = ?",request.ID)
 		// SELECT * FROM `submissions` WHERE request_id = ?
+		// 特定のidを持つclientを抽出する。
+		model.Db.Find(&client,"id = ?",request.ClientID)
+		// SELECT * FROM `clients` WHERE id = ?
+		// 特定のidを持つprofileを抽出する。
+		model.Db.Find(&client_profile,"id = ?",request.ClientID)
+		// SELECT * FROM `profiles` WHERE id = ?
 		// Associationによって、engineerデータを取り出す。
 		model.Db.Model(&request).Association("Engineers").Find(&engineers)
 
@@ -63,23 +78,60 @@ func ShowHomepage(c *gin.Context) {
 		request_json.ID = request.ID
 		request_json.RequestName = request.RequestName
 		request_json.CreatedAt = request.CreatedAt
-		request_json.ClientID = request.ClientID
+		request_json.Content = request.Content
+		request_json.ClientProfile.UserID = request.ClientID
+		request_json.ClientProfile.UserName = client.User.Name
+		request_json.ClientProfile.Icon = client_profile.Icon
+		request_json.ClientProfile.Bio = client_profile.Bio
+		request_json.ClientProfile.Sns = string(client_profile.Sns)
 		request_json.Content = request.Content
 		// EngineersIDは複数存在するため、エンジニアのIDをループで追加していく。
 		for _, engineer := range engineers{
-			request_json.EngineersID = append(request_json.EngineersID,engineer.User.ID)
+			// EngineerのUserProfileJson構造体を格納するためのインスタンスを生成
+			engineer_profile_json := UserProfileJson{}
+			// EngineerのProfile構造体を格納するためのインスタンスを生成
+			engineer_profile := typefile.Profile{}
+
+			// 特定のidを持つprofileを抽出する。
+			model.Db.Find(&engineer_profile,"id = ?",engineer.User.ID)
+			// SELECT * FROM `profiles` WHERE id = ?
+
+			// requestが持つデータをrequest_jsonのそれぞれの対応する属性に格納する。
+			engineer_profile_json.UserID = engineer.User.ID
+			engineer_profile_json.UserName = engineer.User.Name
+			engineer_profile_json.Icon = engineer_profile.Icon
+			engineer_profile_json.Bio = engineer_profile.Bio
+			engineer_profile_json.Sns = string(engineer_profile.Sns)
+
+			request_json.EngineersProfile = append(request_json.EngineersProfile,engineer_profile_json)
 		}
 		// submissionは複数存在するため、submissionデータをループで追加していく。
 		for _, submission := range submissions{
 			// SubmissionJson構造体データを格納するためのインスタンスを生成
 			submission_json := SubmissionJson{}
+			// Engineer構造体を格納するためのインスタンスを生成
+			engineer := typefile.Engineer{}
+			// EngineerのProfile構造体を格納するためのインスタンスを生成
+			engineer_profile := typefile.Profile{}
+
+			// 特定のidを持つengineerを抽出する。
+			model.Db.Find(&engineer,"id = ?",submission.EngineerID)
+			// SELECT * FROM `engineers` WHERE id = ?
+			// 特定のidを持つprofileを抽出する。
+			model.Db.Find(&engineer_profile,"id = ?",submission.EngineerID)
+			// SELECT * FROM `profiles` WHERE id = ?
 
 			// submissionが持つデータをsubmission_jsonのそれぞれの対応する属性に格納する。
 			submission_json.ID = submission.ID
 			submission_json.CreatedAt = submission.CreatedAt
-			submission_json.EngineerID = submission.EngineerID
 			submission_json.URL = submission.URL
+			submission_json.EngineerProfile.UserID = engineer.User.ID
+			submission_json.EngineerProfile.UserName = engineer.User.Name
+			submission_json.EngineerProfile.Icon = engineer_profile.Icon
+			submission_json.EngineerProfile.Bio = engineer_profile.Bio
+			submission_json.EngineerProfile.Sns = string(engineer_profile.Sns)
 			submission_json.Content = submission.Content
+
 			request_json.Submissions = append(request_json.Submissions,submission_json)
 		}
 		requests_json.Requests = append(requests_json.Requests,request_json)
